a50adabb4e04adb1a06c0d76af542dcb8bc2f16c
AngelP00 <92890808+AngelP00@users.noreply.github.com>
2021-10-20 18:54:13 -0300

    Add files via upload

---
+++ TP_Final-F.D.Programacion-Primer-anio/TP_Final_F.D.Programacion_Primer_anio.pas
@@ -0,0 +1,5 @@
+program TP_Final_F.D.Programacion_Primer_anio;
+Uses Unit_TP_Final_F.D.Programacion_Primer_anio;
+begin
+  
+end.
---
+++ TP_Final-F.D.Programacion-Primer-anio/Unit_TP_Final_F.D.Programacion_Primer_anio.pas
@@ -0,0 +1,8 @@
+unit Unit_TP_Final_F.D.Programacion_Primer_anio;
+interface
+
+implementation
+
+begin
+  
+end.
