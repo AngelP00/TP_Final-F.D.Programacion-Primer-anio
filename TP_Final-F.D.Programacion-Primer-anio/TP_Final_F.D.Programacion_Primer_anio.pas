d29d63a4e58d848e18d43b51ce3dff107f095828
AngelP00 <angelpalacios01234@gmail.com>
2021-10-20 19:41:21 -0300

    Update TP_Final_F.D.Programacion_Primer_anio.pas

--- TP_Final-F.D.Programacion-Primer-anio/TP_Final_F.D.Programacion_Primer_anio.pas
+++ TP_Final-F.D.Programacion-Primer-anio/TP_Final_F.D.Programacion_Primer_anio.pas
@@ -3,4 +3,11 @@ Uses Unit_TP_Final_F.D.Programacion_Primer_anio;
 begin
   WriteLn('Modificacion 00');
   WriteLn('Modificacion 01');
+  ReadLn(var_01);
+  if(var_01 = 2)then
+  begin
+    WriteLn('Es el numero 2');
+  end
+  else
+    WriteLn('No es el numero 2');
 end.
