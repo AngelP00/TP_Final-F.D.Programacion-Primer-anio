c3cc65f9eb684b55153be6f4e7bc0cdd275fea44
AngelP00 <angelpalacios01234@gmail.com>
2021-10-22 15:15:26 -0300

    Update TP_Final_F.D.Programacion_Primer_anio.pas

--- TP_Final-F.D.Programacion-Primer-anio/TP_Final_F.D.Programacion_Primer_anio.pas
+++ TP_Final-F.D.Programacion-Primer-anio/TP_Final_F.D.Programacion_Primer_anio.pas
@@ -2,6 +2,5 @@ program TP_Final_F.D.Programacion_Primer_anio;
 Uses Unit_TP_Final_F.D.Programacion_Primer_anio;
 Var var_01:Integer;
 begin
-  WriteLn('Prueba__');
   WriteLn('Prueba_para eliminar linea');
 end.
