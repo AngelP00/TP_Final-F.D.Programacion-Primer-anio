b2b0ce2de1ec1f0e275d05a5b403e607c798d14c
AngelP00 <angelpalacios01234@gmail.com>
2021-10-22 15:09:23 -0300

    Update TP_Final_F.D.Programacion_Primer_anio.pas

--- TP_Final-F.D.Programacion-Primer-anio/TP_Final_F.D.Programacion_Primer_anio.pas
+++ TP_Final-F.D.Programacion-Primer-anio/TP_Final_F.D.Programacion_Primer_anio.pas
@@ -2,17 +2,5 @@ program TP_Final_F.D.Programacion_Primer_anio;
 Uses Unit_TP_Final_F.D.Programacion_Primer_anio;
 Var var_01:Integer;
 begin
-  WriteLn('Modificacion 00');
-  WriteLn('Modificacion 01');
-  ReadLn(var_01);
-  if(var_01 = 2)then
-  begin
-    WriteLn('Es el numero 2');
-  end
-  else
-    WriteLn('No es el numero 2');
-    //WriteLn('Hi 00(Para probar la edicion)');
-    WriteLn('Hi 00(Para probar la edicion01)');
-    WriteLn('Hi 01');
-    WriteLn('Hi 02');
+  
 end.
